fbf473f2b565b901cb49cda4405929079d035970
rian <19516527+360ied@users.noreply.github.com>
2020-08-31 20:29:38 -0400

    register event handlers

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	
+	registerEventHandlers(bot)
 
 	err = bot.Open()
 
@@ -33,6 +35,53 @@ func main() {
 
 	// unreachable code
 	// bot.Close()
+}
+
+func registerEventHandlers(bot *discordgo.Session) {
+	bot.AddHandler(channelCreateEventHandler)
+	bot.AddHandler(channelDeleteEventHandler)
+	bot.AddHandler(channelPinsUpdateEventHandler)
+	bot.AddHandler(channelUpdateEventHandler)
+	bot.AddHandler(connectEventHandler)
+	bot.AddHandler(disconnectEventHandler)
+	bot.AddHandler(eventEventHandler)
+	bot.AddHandler(guildBanAddEventHandler)
+	bot.AddHandler(guildBanRemoveEventHandler)
+	bot.AddHandler(guildCreateEventHandler)
+	bot.AddHandler(guildDeleteEventHandler)
+	bot.AddHandler(guildEmojisUpdateEventHandler)
+	bot.AddHandler(guildIntegrationsUpdateEventHandler)
+	bot.AddHandler(guildMemberAddEventHandler)
+	bot.AddHandler(guildMemberRemoveEventHandler)
+	bot.AddHandler(guildMemberUpdateEventHandler)
+	bot.AddHandler(guildMembersChunkEventHandler)
+	bot.AddHandler(guildRolesCreateEventHandler)
+	bot.AddHandler(guildRoleDeleteEventHandler)
+	bot.AddHandler(guildRoleUpdateEventHandler)
+	bot.AddHandler(guildUpdateEventHandler)
+	bot.AddHandler(messageAckEventHandler)
+	bot.AddHandler(messageCreateEventHandler)
+	bot.AddHandler(messageDeleteEventHandler)
+	bot.AddHandler(messageDeleteBulkEventHandler)
+	bot.AddHandler(messageReactionAddEventHandler)
+	bot.AddHandler(messageReactionRemoveEventHandler)
+	bot.AddHandler(messageReactionRemoveAllEventHandler)
+	bot.AddHandler(messageUpdateEventHandler)
+	bot.AddHandler(presenceUpdateEventHandler)
+	bot.AddHandler(presencesReplaceEventHandler)
+	bot.AddHandler(rateLimitEventHandler)
+	bot.AddHandler(readyEventHandler)
+	bot.AddHandler(relationshipAddEventHandler)
+	bot.AddHandler(relationshipRemoveEventHandler)
+	bot.AddHandler(resumedEventHandler)
+	bot.AddHandler(typingStartEventHandler)
+	bot.AddHandler(userGuildSettingsUpdateEventHandler)
+	bot.AddHandler(userNoteUpdateEventHandler)
+	bot.AddHandler(userSettingsUpdateEventHandler)
+	bot.AddHandler(userUpdateEventHandler)
+	bot.AddHandler(voiceServerUpdateEventHandler)
+	bot.AddHandler(voiceStateUpdateEventHandler)
+	bot.AddHandler(webhooksUpdateEventHandler)
 }
 
 func channelCreateEventHandler(session *discordgo.Session, event *discordgo.ChannelCreate) {}
