068414d5668b60a143770077dbbf18f7c9fe4df0
rian <19516527+360ied@users.noreply.github.com>
2020-08-31 20:39:51 -0400

    move registration and event handlers into their own package

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 
+	"rpdSeven/discordextended"
 	"rpdSeven/keepalive"
 )
 
@@ -20,7 +21,7 @@ func main() {
 		panic(err)
 	}
 
-	registerEventHandlers(bot)
+	discordextended.RegisterEventHandlers(bot)
 
 	err = bot.Open()
 
@@ -36,142 +37,3 @@ func main() {
 	// unreachable code
 	// bot.Close()
 }
-
-func registerEventHandlers(bot *discordgo.Session) {
-	bot.AddHandler(channelCreateEventHandler)
-	bot.AddHandler(channelDeleteEventHandler)
-	bot.AddHandler(channelPinsUpdateEventHandler)
-	bot.AddHandler(channelUpdateEventHandler)
-	bot.AddHandler(connectEventHandler)
-	bot.AddHandler(disconnectEventHandler)
-	bot.AddHandler(eventEventHandler)
-	bot.AddHandler(guildBanAddEventHandler)
-	bot.AddHandler(guildBanRemoveEventHandler)
-	bot.AddHandler(guildCreateEventHandler)
-	bot.AddHandler(guildDeleteEventHandler)
-	bot.AddHandler(guildEmojisUpdateEventHandler)
-	bot.AddHandler(guildIntegrationsUpdateEventHandler)
-	bot.AddHandler(guildMemberAddEventHandler)
-	bot.AddHandler(guildMemberRemoveEventHandler)
-	bot.AddHandler(guildMemberUpdateEventHandler)
-	bot.AddHandler(guildMembersChunkEventHandler)
-	bot.AddHandler(guildRoleCreateEventHandler)
-	bot.AddHandler(guildRoleDeleteEventHandler)
-	bot.AddHandler(guildRoleUpdateEventHandler)
-	bot.AddHandler(guildUpdateEventHandler)
-	bot.AddHandler(messageAckEventHandler)
-	bot.AddHandler(messageCreateEventHandler)
-	bot.AddHandler(messageDeleteEventHandler)
-	bot.AddHandler(messageDeleteBulkEventHandler)
-	bot.AddHandler(messageReactionAddEventHandler)
-	bot.AddHandler(messageReactionRemoveEventHandler)
-	bot.AddHandler(messageReactionRemoveAllEventHandler)
-	bot.AddHandler(messageUpdateEventHandler)
-	bot.AddHandler(presenceUpdateEventHandler)
-	bot.AddHandler(presencesReplaceEventHandler)
-	bot.AddHandler(rateLimitEventHandler)
-	bot.AddHandler(readyEventHandler)
-	bot.AddHandler(relationshipAddEventHandler)
-	bot.AddHandler(relationshipRemoveEventHandler)
-	bot.AddHandler(resumedEventHandler)
-	bot.AddHandler(typingStartEventHandler)
-	bot.AddHandler(userGuildSettingsUpdateEventHandler)
-	bot.AddHandler(userNoteUpdateEventHandler)
-	bot.AddHandler(userSettingsUpdateEventHandler)
-	bot.AddHandler(userUpdateEventHandler)
-	bot.AddHandler(voiceServerUpdateEventHandler)
-	bot.AddHandler(voiceStateUpdateEventHandler)
-	bot.AddHandler(webhooksUpdateEventHandler)
-}
-
-func channelCreateEventHandler(session *discordgo.Session, event *discordgo.ChannelCreate) {}
-
-func channelDeleteEventHandler(session *discordgo.Session, event *discordgo.ChannelCreate) {}
-
-func channelPinsUpdateEventHandler(session *discordgo.Session, event *discordgo.ChannelPinsUpdate) {}
-
-func channelUpdateEventHandler(session *discordgo.Session, event *discordgo.ChannelUpdate) {}
-
-func connectEventHandler(session *discordgo.Session, event *discordgo.Connect) {}
-
-func disconnectEventHandler(session *discordgo.Session, event *discordgo.Disconnect) {}
-
-func eventEventHandler(session *discordgo.Session, event *discordgo.Event) {}
-
-func guildBanAddEventHandler(session *discordgo.Session, event *discordgo.GuildBanAdd) {}
-
-func guildBanRemoveEventHandler(session *discordgo.Session, event *discordgo.GuildBanRemove) {}
-
-func guildCreateEventHandler(session *discordgo.Session, event *discordgo.GuildCreate) {}
-
-func guildDeleteEventHandler(session *discordgo.Session, event *discordgo.GuildDelete) {}
-
-func guildEmojisUpdateEventHandler(session *discordgo.Session, event *discordgo.GuildEmojisUpdate) {}
-
-func guildIntegrationsUpdateEventHandler(session *discordgo.Session, event *discordgo.GuildIntegrationsUpdate) {
-}
-
-func guildMemberAddEventHandler(session *discordgo.Session, event *discordgo.GuildMemberAdd) {}
-
-func guildMemberRemoveEventHandler(session *discordgo.Session, event *discordgo.GuildMemberRemove) {}
-
-func guildMemberUpdateEventHandler(session *discordgo.Session, event *discordgo.GuildMemberUpdate) {}
-
-func guildMembersChunkEventHandler(session *discordgo.Session, event *discordgo.GuildMembersChunk) {}
-
-func guildRoleCreateEventHandler(session *discordgo.Session, event *discordgo.GuildRoleCreate) {}
-
-func guildRoleDeleteEventHandler(session *discordgo.Session, event *discordgo.GuildRoleDelete) {}
-
-func guildRoleUpdateEventHandler(session *discordgo.Session, event *discordgo.GuildRoleUpdate) {}
-
-func guildUpdateEventHandler(session *discordgo.Session, event *discordgo.GuildUpdate) {}
-
-func messageAckEventHandler(session *discordgo.Session, event *discordgo.MessageAck) {}
-
-func messageCreateEventHandler(session *discordgo.Session, event *discordgo.MessageCreate) {}
-
-func messageDeleteEventHandler(session *discordgo.Session, event *discordgo.MessageDelete) {}
-
-func messageDeleteBulkEventHandler(session *discordgo.Session, event *discordgo.MessageDeleteBulk) {}
-
-func messageReactionAddEventHandler(session *discordgo.Session, event *discordgo.MessageReactionAdd) {}
-
-func messageReactionRemoveEventHandler(session *discordgo.Session, event *discordgo.MessageReactionRemove) {
-}
-
-func messageReactionRemoveAllEventHandler(session *discordgo.Session, event *discordgo.MessageReactionRemoveAll) {
-}
-
-func messageUpdateEventHandler(session *discordgo.Session, event *discordgo.MessageUpdate) {}
-
-func presenceUpdateEventHandler(session *discordgo.Session, event *discordgo.PresenceUpdate) {}
-
-func presencesReplaceEventHandler(session *discordgo.Session, event *discordgo.PresencesReplace) {}
-
-func rateLimitEventHandler(session *discordgo.Session, event *discordgo.RateLimit) {}
-
-func readyEventHandler(session *discordgo.Session, event *discordgo.Ready) {}
-
-func relationshipAddEventHandler(session *discordgo.Session, event *discordgo.RelationshipAdd) {}
-
-func relationshipRemoveEventHandler(session *discordgo.Session, event *discordgo.RelationshipRemove) {}
-
-func resumedEventHandler(session *discordgo.Session, event *discordgo.Resumed) {}
-
-func typingStartEventHandler(session *discordgo.Session, event *discordgo.TypingStart) {}
-
-func userGuildSettingsUpdateEventHandler(session *discordgo.Session, event *discordgo.UserGuildSettingsUpdate) {
-}
-
-func userNoteUpdateEventHandler(session *discordgo.Session, event *discordgo.UserNoteUpdate) {}
-
-func userSettingsUpdateEventHandler(session *discordgo.Session, event *discordgo.UserSettingsUpdate) {}
-
-func userUpdateEventHandler(session *discordgo.Session, event *discordgo.UserUpdate) {}
-
-func voiceServerUpdateEventHandler(session *discordgo.Session, event *discordgo.VoiceServerUpdate) {}
-
-func voiceStateUpdateEventHandler(session *discordgo.Session, event *discordgo.VoiceStateUpdate) {}
-
-func webhooksUpdateEventHandler(session *discordgo.Session, event *discordgo.WebhooksUpdate) {}
