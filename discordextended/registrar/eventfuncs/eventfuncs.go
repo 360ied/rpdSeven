3acf11b7062fff517388178ceeb00033f0e04921
rian <19516527+360ied@users.noreply.github.com>
2020-09-02 11:48:54 -0400

    add thread safe event register functions

--- discordextended/registrar/eventfuncs/eventfuncs.go
+++ discordextended/registrar/eventfuncs/eventfuncs.go
@@ -1,95 +1,141 @@
-package ev
+package eventfuncs
 
 import (
+	"sync"
+
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
+	ChannelCreateEventMutex    = sync.Mutex{}
 	ChannelCreateEventHandlers []func(*discordgo.Session, *discordgo.ChannelCreate)
 
+	ChannelDeleteEventMutex    = sync.Mutex{}
 	ChannelDeleteEventHandlers []func(*discordgo.Session, *discordgo.ChannelDelete)
 
+	ChannelPinsUpdateEventMutex    = sync.Mutex{}
 	ChannelPinsUpdateEventHandlers []func(*discordgo.Session, *discordgo.ChannelPinsUpdate)
 
+	ChannelUpdateEventMutex    = sync.Mutex{}
 	ChannelUpdateEventHandlers []func(*discordgo.Session, *discordgo.ChannelUpdate)
 
+	ConnectEventMutex    = sync.Mutex{}
 	ConnectEventHandlers []func(*discordgo.Session, *discordgo.Connect)
 
+	DisconnectEventMutex    = sync.Mutex{}
 	DisconnectEventHandlers []func(*discordgo.Session, *discordgo.Disconnect)
 
+	EventEventMutex    = sync.Mutex{}
 	EventEventHandlers []func(*discordgo.Session, *discordgo.Event)
 
+	GuildBanAddEventMutex    = sync.Mutex{}
 	GuildBanAddEventHandlers []func(*discordgo.Session, *discordgo.GuildBanAdd)
 
+	GuildBanRemoveEventMutex    = sync.Mutex{}
 	GuildBanRemoveEventHandlers []func(*discordgo.Session, *discordgo.GuildBanRemove)
 
+	GuildCreateEventMutex    = sync.Mutex{}
 	GuildCreateEventHandlers []func(*discordgo.Session, *discordgo.GuildCreate)
 
+	GuildDeleteEventMutex    = sync.Mutex{}
 	GuildDeleteEventHandlers []func(*discordgo.Session, *discordgo.GuildDelete)
 
+	GuildEmojisUpdateEventMutex    = sync.Mutex{}
 	GuildEmojisUpdateEventHandlers []func(*discordgo.Session, *discordgo.GuildEmojisUpdate)
 
+	GuildIntegrationsUpdateEventMutex    = sync.Mutex{}
 	GuildIntegrationsUpdateEventHandlers []func(*discordgo.Session, *discordgo.GuildIntegrationsUpdate)
 
+	GuildMemberAddEventMutex    = sync.Mutex{}
 	GuildMemberAddEventHandlers []func(*discordgo.Session, *discordgo.GuildMemberAdd)
 
+	GuildMemberRemoveEventMutex    = sync.Mutex{}
 	GuildMemberRemoveEventHandlers []func(*discordgo.Session, *discordgo.GuildMemberRemove)
 
+	GuildMemberUpdateEventMutex    = sync.Mutex{}
 	GuildMemberUpdateEventHandlers []func(*discordgo.Session, *discordgo.GuildMemberUpdate)
 
+	GuildMembersChunkEventMutex    = sync.Mutex{}
 	GuildMembersChunkEventHandlers []func(*discordgo.Session, *discordgo.GuildMembersChunk)
 
+	GuildRoleCreateEventMutex    = sync.Mutex{}
 	GuildRoleCreateEventHandlers []func(*discordgo.Session, *discordgo.GuildRoleCreate)
 
+	GuildRoleDeleteEventMutex    = sync.Mutex{}
 	GuildRoleDeleteEventHandlers []func(*discordgo.Session, *discordgo.GuildRoleDelete)
 
+	GuildRoleUpdateEventMutex    = sync.Mutex{}
 	GuildRoleUpdateEventHandlers []func(*discordgo.Session, *discordgo.GuildRoleUpdate)
 
+	GuildUpdateEventMutex    = sync.Mutex{}
 	GuildUpdateEventHandlers []func(*discordgo.Session, *discordgo.GuildUpdate)
 
+	MessageAckEventMutex    = sync.Mutex{}
 	MessageAckEventHandlers []func(*discordgo.Session, *discordgo.MessageAck)
 
+	MessageCreateEventMutex    = sync.Mutex{}
 	MessageCreateEventHandlers []func(*discordgo.Session, *discordgo.MessageCreate)
 
+	MessageDeleteEventMutex    = sync.Mutex{}
 	MessageDeleteEventHandlers []func(*discordgo.Session, *discordgo.MessageDelete)
 
+	MessageDeleteBulkEventMutex    = sync.Mutex{}
 	MessageDeleteBulkEventHandlers []func(*discordgo.Session, *discordgo.MessageDeleteBulk)
 
+	MessageReactionAddEventMutex    = sync.Mutex{}
 	MessageReactionAddEventHandlers []func(*discordgo.Session, *discordgo.MessageReactionAdd)
 
+	MessageReactionRemoveEventMutex    = sync.Mutex{}
 	MessageReactionRemoveEventHandlers []func(*discordgo.Session, *discordgo.MessageReactionRemove)
 
+	MessageReactionRemoveAllEventMutex    = sync.Mutex{}
 	MessageReactionRemoveAllEventHandlers []func(*discordgo.Session, *discordgo.MessageReactionRemoveAll)
 
+	MessageUpdateEventMutex    = sync.Mutex{}
 	MessageUpdateEventHandlers []func(*discordgo.Session, *discordgo.MessageUpdate)
 
+	PresenceUpdateEventMutex    = sync.Mutex{}
 	PresenceUpdateEventHandlers []func(*discordgo.Session, *discordgo.PresenceUpdate)
 
+	PresencesReplaceEventMutex    = sync.Mutex{}
 	PresencesReplaceEventHandlers []func(*discordgo.Session, *discordgo.PresencesReplace)
 
+	RateLimitEventMutex    = sync.Mutex{}
 	RateLimitEventHandlers []func(*discordgo.Session, *discordgo.RateLimit)
 
+	ReadyEventMutex    = sync.Mutex{}
 	ReadyEventHandlers []func(*discordgo.Session, *discordgo.Ready)
 
+	RelationshipAddEventMutex    = sync.Mutex{}
 	RelationshipAddEventHandlers []func(*discordgo.Session, *discordgo.RelationshipAdd)
 
+	RelationshipRemoveEventMutex    = sync.Mutex{}
 	RelationshipRemoveEventHandlers []func(*discordgo.Session, *discordgo.RelationshipRemove)
 
+	ResumedEventMutex    = sync.Mutex{}
 	ResumedEventHandlers []func(*discordgo.Session, *discordgo.Resumed)
 
+	TypingStartEventMutex    = sync.Mutex{}
 	TypingStartEventHandlers []func(*discordgo.Session, *discordgo.TypingStart)
 
+	UserGuildSettingsUpdateEventMutex    = sync.Mutex{}
 	UserGuildSettingsUpdateEventHandlers []func(*discordgo.Session, *discordgo.UserGuildSettingsUpdate)
 
+	UserNoteUpdateEventMutex    = sync.Mutex{}
 	UserNoteUpdateEventHandlers []func(*discordgo.Session, *discordgo.UserNoteUpdate)
 
+	UserSettingsUpdateEventMutex    = sync.Mutex{}
 	UserSettingsUpdateEventHandlers []func(*discordgo.Session, *discordgo.UserSettingsUpdate)
 
+	UserUpdateEventMutex    = sync.Mutex{}
 	UserUpdateEventHandlers []func(*discordgo.Session, *discordgo.UserUpdate)
 
+	VoiceServerUpdateEventMutex    = sync.Mutex{}
 	VoiceServerUpdateEventHandlers []func(*discordgo.Session, *discordgo.VoiceServerUpdate)
 
+	VoiceStateUpdateEventMutex    = sync.Mutex{}
 	VoiceStateUpdateEventHandlers []func(*discordgo.Session, *discordgo.VoiceStateUpdate)
 
+	WebhooksUpdateEventMutex    = sync.Mutex{}
 	WebhooksUpdateEventHandlers []func(*discordgo.Session, *discordgo.WebhooksUpdate)
 )
--- discordextended/registrar/eventregistrar.go
+++ discordextended/registrar/eventregistrar.go
@@ -2,131 +2,268 @@ package registrar
 
 import (
 	"github.com/bwmarrin/discordgo"
+
+	"rpdSeven/discordextended/registrar/eventfuncs"
 )
 
-func RegisterChannelCreateEvent(function func(session *discordgo.Session, event *discordgo.ChannelCreate)) {
+func RegisterChannelCreateEvent(function func(*discordgo.Session, *discordgo.ChannelCreate)) {
+	eventfuncs.ChannelCreateEventMutex.Lock()
+	defer eventfuncs.ChannelCreateEventMutex.Unlock()
+	eventfuncs.ChannelCreateEventHandlers = append(eventfuncs.ChannelCreateEventHandlers, function)
 }
 
-func RegisterChannelDeleteEventHandler(function func(session *discordgo.Session, event *discordgo.ChannelCreate)) {
+func RegisterChannelDeleteEventHandler(function func(*discordgo.Session, *discordgo.ChannelDelete)) {
+	eventfuncs.ChannelDeleteEventMutex.Lock()
+	defer eventfuncs.ChannelDeleteEventMutex.Unlock()
+	eventfuncs.ChannelDeleteEventHandlers = append(eventfuncs.ChannelDeleteEventHandlers, function)
 }
 
-func RegisterChannelPinsUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.ChannelPinsUpdate)) {
+func RegisterChannelPinsUpdateEventHandler(function func(*discordgo.Session, *discordgo.ChannelPinsUpdate)) {
+	eventfuncs.ChannelPinsUpdateEventMutex.Lock()
+	defer eventfuncs.ChannelPinsUpdateEventMutex.Unlock()
+	eventfuncs.ChannelPinsUpdateEventHandlers = append(eventfuncs.ChannelPinsUpdateEventHandlers, function)
 }
 
-func RegisterChannelUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.ChannelUpdate)) {
+func RegisterChannelUpdateEventHandler(function func(*discordgo.Session, *discordgo.ChannelUpdate)) {
+	eventfuncs.ChannelUpdateEventMutex.Lock()
+	defer eventfuncs.ChannelUpdateEventMutex.Unlock()
+	eventfuncs.ChannelUpdateEventHandlers = append(eventfuncs.ChannelUpdateEventHandlers, function)
 }
 
-func RegisterConnectEventHandler(function func(session *discordgo.Session, event *discordgo.Connect)) {
+func RegisterConnectEventHandler(function func(*discordgo.Session, *discordgo.Connect)) {
+	eventfuncs.ConnectEventMutex.Lock()
+	defer eventfuncs.ConnectEventMutex.Unlock()
+	eventfuncs.ConnectEventHandlers = append(eventfuncs.ConnectEventHandlers, function)
 }
 
-func RegisterDisconnectEventHandler(function func(session *discordgo.Session, event *discordgo.Disconnect)) {
+func RegisterDisconnectEventHandler(function func(*discordgo.Session, *discordgo.Disconnect)) {
+	eventfuncs.DisconnectEventMutex.Lock()
+	defer eventfuncs.DisconnectEventMutex.Unlock()
+	eventfuncs.DisconnectEventHandlers = append(eventfuncs.DisconnectEventHandlers, function)
 }
 
-func RegisterEventEventHandler(function func(session *discordgo.Session, event *discordgo.Event)) {}
-
-func RegisterGuildBanAddEventHandler(function func(session *discordgo.Session, event *discordgo.GuildBanAdd)) {
+func RegisterEventEventHandler(function func(*discordgo.Session, *discordgo.Event)) {
+	eventfuncs.EventEventMutex.Lock()
+	defer eventfuncs.EventEventMutex.Unlock()
+	eventfuncs.EventEventHandlers = append(eventfuncs.EventEventHandlers, function)
 }
 
-func RegisterGuildBanRemoveEventHandler(function func(session *discordgo.Session, event *discordgo.GuildBanRemove)) {
+func RegisterGuildBanAddEventHandler(function func(*discordgo.Session, *discordgo.GuildBanAdd)) {
+	eventfuncs.GuildBanAddEventMutex.Lock()
+	defer eventfuncs.GuildBanAddEventMutex.Unlock()
+	eventfuncs.GuildBanAddEventHandlers = append(eventfuncs.GuildBanAddEventHandlers, function)
 }
 
-func RegisterGuildCreateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildCreate)) {
+func RegisterGuildBanRemoveEventHandler(function func(*discordgo.Session, *discordgo.GuildBanRemove)) {
+	eventfuncs.GuildBanRemoveEventMutex.Lock()
+	defer eventfuncs.GuildBanRemoveEventMutex.Unlock()
+	eventfuncs.GuildBanRemoveEventHandlers = append(eventfuncs.GuildBanRemoveEventHandlers, function)
 }
 
-func RegisterGuildDeleteEventHandler(function func(session *discordgo.Session, event *discordgo.GuildDelete)) {
+func RegisterGuildCreateEventHandler(function func(*discordgo.Session, *discordgo.GuildCreate)) {
+	eventfuncs.GuildCreateEventMutex.Lock()
+	defer eventfuncs.GuildCreateEventMutex.Unlock()
+	eventfuncs.GuildCreateEventHandlers = append(eventfuncs.GuildCreateEventHandlers, function)
 }
 
-func RegisterGuildEmojisUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildEmojisUpdate)) {
+func RegisterGuildDeleteEventHandler(function func(*discordgo.Session, *discordgo.GuildDelete)) {
+	eventfuncs.GuildDeleteEventMutex.Lock()
+	defer eventfuncs.GuildDeleteEventMutex.Unlock()
+	eventfuncs.GuildDeleteEventHandlers = append(eventfuncs.GuildDeleteEventHandlers, function)
 }
 
-func RegisterGuildIntegrationsUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildIntegrationsUpdate)) {
-}
-func RegisterGuildMemberAddEventHandler(function func(session *discordgo.Session, event *discordgo.GuildMemberAdd)) {
-}
-
-func RegisterGuildMemberRemoveEventHandler(function func(session *discordgo.Session, event *discordgo.GuildMemberRemove)) {
+func RegisterGuildEmojisUpdateEventHandler(function func(*discordgo.Session, *discordgo.GuildEmojisUpdate)) {
+	eventfuncs.GuildEmojisUpdateEventMutex.Lock()
+	defer eventfuncs.GuildEmojisUpdateEventMutex.Unlock()
+	eventfuncs.GuildEmojisUpdateEventHandlers = append(eventfuncs.GuildEmojisUpdateEventHandlers, function)
 }
 
-func RegisterGuildMemberUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildMemberUpdate)) {
+func RegisterGuildIntegrationsUpdateEventHandler(function func(*discordgo.Session, *discordgo.GuildIntegrationsUpdate)) {
+	eventfuncs.GuildIntegrationsUpdateEventMutex.Lock()
+	defer eventfuncs.GuildIntegrationsUpdateEventMutex.Unlock()
+	eventfuncs.GuildIntegrationsUpdateEventHandlers = append(eventfuncs.GuildIntegrationsUpdateEventHandlers, function)
+
+}
+func RegisterGuildMemberAddEventHandler(function func(*discordgo.Session, *discordgo.GuildMemberAdd)) {
+	eventfuncs.GuildMemberAddEventMutex.Lock()
+	defer eventfuncs.GuildMemberAddEventMutex.Unlock()
+	eventfuncs.GuildMemberAddEventHandlers = append(eventfuncs.GuildMemberAddEventHandlers, function)
 }
 
-func RegisterGuildMembersChunkEventHandler(function func(session *discordgo.Session, event *discordgo.GuildMembersChunk)) {
+func RegisterGuildMemberRemoveEventHandler(function func(*discordgo.Session, *discordgo.GuildMemberRemove)) {
+	eventfuncs.GuildMemberRemoveEventMutex.Lock()
+	defer eventfuncs.GuildMemberRemoveEventMutex.Unlock()
+	eventfuncs.GuildMemberRemoveEventHandlers = append(eventfuncs.GuildMemberRemoveEventHandlers, function)
 }
 
-func RegisterGuildRoleCreateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildRoleCreate)) {
+func RegisterGuildMemberUpdateEventHandler(function func(*discordgo.Session, *discordgo.GuildMemberUpdate)) {
+	eventfuncs.GuildMemberUpdateEventMutex.Lock()
+	defer eventfuncs.GuildMemberUpdateEventMutex.Unlock()
+	eventfuncs.GuildMemberUpdateEventHandlers = append(eventfuncs.GuildMemberUpdateEventHandlers, function)
 }
 
-func RegisterGuildRoleDeleteEventHandler(function func(session *discordgo.Session, event *discordgo.GuildRoleDelete)) {
+func RegisterGuildMembersChunkEventHandler(function func(*discordgo.Session, *discordgo.GuildMembersChunk)) {
+	eventfuncs.GuildMembersChunkEventMutex.Lock()
+	defer eventfuncs.GuildMembersChunkEventMutex.Unlock()
+	eventfuncs.GuildMembersChunkEventHandlers = append(eventfuncs.GuildMembersChunkEventHandlers, function)
 }
 
-func RegisterGuildRoleUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildRoleUpdate)) {
+func RegisterGuildRoleCreateEventHandler(function func(*discordgo.Session, *discordgo.GuildRoleCreate)) {
+	eventfuncs.GuildRoleCreateEventMutex.Lock()
+	defer eventfuncs.GuildRoleCreateEventMutex.Unlock()
+	eventfuncs.GuildRoleCreateEventHandlers = append(eventfuncs.GuildRoleCreateEventHandlers, function)
 }
 
-func RegisterGuildUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.GuildUpdate)) {
+func RegisterGuildRoleDeleteEventHandler(function func(*discordgo.Session, *discordgo.GuildRoleDelete)) {
+	eventfuncs.GuildRoleDeleteEventMutex.Lock()
+	defer eventfuncs.GuildRoleDeleteEventMutex.Unlock()
+	eventfuncs.GuildRoleDeleteEventHandlers = append(eventfuncs.GuildRoleDeleteEventHandlers, function)
 }
 
-func RegisterMessageAckEventHandler(function func(session *discordgo.Session, event *discordgo.MessageAck)) {
+func RegisterGuildRoleUpdateEventHandler(function func(*discordgo.Session, *discordgo.GuildRoleUpdate)) {
+	eventfuncs.GuildRoleUpdateEventMutex.Lock()
+	defer eventfuncs.GuildRoleUpdateEventMutex.Unlock()
+	eventfuncs.GuildRoleUpdateEventHandlers = append(eventfuncs.GuildRoleUpdateEventHandlers, function)
 }
 
-func RegisterMessageCreateEventHandler(function func(session *discordgo.Session, event *discordgo.MessageCreate)) {
+func RegisterGuildUpdateEventHandler(function func(*discordgo.Session, *discordgo.GuildUpdate)) {
+	eventfuncs.GuildUpdateEventMutex.Lock()
+	defer eventfuncs.GuildUpdateEventMutex.Unlock()
+	eventfuncs.GuildUpdateEventHandlers = append(eventfuncs.GuildUpdateEventHandlers, function)
 }
 
-func RegisterMessageDeleteEventHandler(function func(session *discordgo.Session, event *discordgo.MessageDelete)) {
+func RegisterMessageAckEventHandler(function func(*discordgo.Session, *discordgo.MessageAck)) {
+	eventfuncs.MessageAckEventMutex.Lock()
+	defer eventfuncs.MessageAckEventMutex.Unlock()
+	eventfuncs.MessageAckEventHandlers = append(eventfuncs.MessageAckEventHandlers, function)
 }
 
-func RegisterMessageDeleteBulkEventHandler(function func(session *discordgo.Session, event *discordgo.MessageDeleteBulk)) {
+func RegisterMessageCreateEventHandler(function func(*discordgo.Session, *discordgo.MessageCreate)) {
+	eventfuncs.MessageCreateEventMutex.Lock()
+	defer eventfuncs.MessageCreateEventMutex.Unlock()
+	eventfuncs.MessageCreateEventHandlers = append(eventfuncs.MessageCreateEventHandlers, function)
 }
 
-func RegisterMessageReactionAddEventHandler(function func(session *discordgo.Session, event *discordgo.MessageReactionAdd)) {
+func RegisterMessageDeleteEventHandler(function func(*discordgo.Session, *discordgo.MessageDelete)) {
+	eventfuncs.MessageDeleteEventMutex.Lock()
+	defer eventfuncs.MessageDeleteEventMutex.Unlock()
+	eventfuncs.MessageDeleteEventHandlers = append(eventfuncs.MessageDeleteEventHandlers, function)
 }
 
-func RegisterMessageReactionRemoveEventHandler(function func(session *discordgo.Session, event *discordgo.MessageReactionRemove)) {
-}
-func RegisterMessageReactionRemoveAllEventHandler(function func(session *discordgo.Session, event *discordgo.MessageReactionRemoveAll)) {
-}
-func RegisterMessageUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.MessageUpdate)) {
-}
-
-func RegisterPresenceUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.PresenceUpdate)) {
+func RegisterMessageDeleteBulkEventHandler(function func(*discordgo.Session, *discordgo.MessageDeleteBulk)) {
+	eventfuncs.MessageDeleteBulkEventMutex.Lock()
+	defer eventfuncs.MessageDeleteBulkEventMutex.Unlock()
+	eventfuncs.MessageDeleteBulkEventHandlers = append(eventfuncs.MessageDeleteBulkEventHandlers, function)
 }
 
-func RegisterPresencesReplaceEventHandler(function func(session *discordgo.Session, event *discordgo.PresencesReplace)) {
+func RegisterMessageReactionAddEventHandler(function func(*discordgo.Session, *discordgo.MessageReactionAdd)) {
+	eventfuncs.MessageReactionAddEventMutex.Lock()
+	defer eventfuncs.MessageReactionAddEventMutex.Unlock()
+	eventfuncs.MessageReactionAddEventHandlers = append(eventfuncs.MessageReactionAddEventHandlers, function)
 }
 
-func RegisterRateLimitEventHandler(function func(session *discordgo.Session, event *discordgo.RateLimit)) {
+func RegisterMessageReactionRemoveEventHandler(function func(*discordgo.Session, *discordgo.MessageReactionRemove)) {
+	eventfuncs.MessageReactionRemoveEventMutex.Lock()
+	defer eventfuncs.MessageReactionRemoveEventMutex.Unlock()
+	eventfuncs.MessageReactionRemoveEventHandlers = append(eventfuncs.MessageReactionRemoveEventHandlers, function)
+}
+func RegisterMessageReactionRemoveAllEventHandler(function func(*discordgo.Session, *discordgo.MessageReactionRemoveAll)) {
+	eventfuncs.MessageReactionRemoveAllEventMutex.Lock()
+	defer eventfuncs.MessageReactionRemoveAllEventMutex.Unlock()
+	eventfuncs.MessageReactionRemoveAllEventHandlers = append(eventfuncs.MessageReactionRemoveAllEventHandlers, function)
+}
+func RegisterMessageUpdateEventHandler(function func(*discordgo.Session, *discordgo.MessageUpdate)) {
+	eventfuncs.MessageUpdateEventMutex.Lock()
+	defer eventfuncs.MessageUpdateEventMutex.Unlock()
+	eventfuncs.MessageUpdateEventHandlers = append(eventfuncs.MessageUpdateEventHandlers, function)
 }
 
-func RegisterReadyEventHandler(function func(session *discordgo.Session, event *discordgo.Ready)) {}
-
-func RegisterRelationshipAddEventHandler(function func(session *discordgo.Session, event *discordgo.RelationshipAdd)) {
+func RegisterPresenceUpdateEventHandler(function func(*discordgo.Session, *discordgo.PresenceUpdate)) {
+	eventfuncs.PresenceUpdateEventMutex.Lock()
+	defer eventfuncs.PresenceUpdateEventMutex.Unlock()
+	eventfuncs.PresenceUpdateEventHandlers = append(eventfuncs.PresenceUpdateEventHandlers, function)
 }
 
-func RegisterRelationshipRemoveEventHandler(function func(session *discordgo.Session, event *discordgo.RelationshipRemove)) {
+func RegisterPresencesReplaceEventHandler(function func(*discordgo.Session, *discordgo.PresencesReplace)) {
+	eventfuncs.PresencesReplaceEventMutex.Lock()
+	defer eventfuncs.PresencesReplaceEventMutex.Unlock()
+	eventfuncs.PresencesReplaceEventHandlers = append(eventfuncs.PresencesReplaceEventHandlers, function)
 }
 
-func RegisterResumedEventHandler(function func(session *discordgo.Session, event *discordgo.Resumed)) {
+func RegisterRateLimitEventHandler(function func(*discordgo.Session, *discordgo.RateLimit)) {
+	eventfuncs.RateLimitEventMutex.Lock()
+	defer eventfuncs.RateLimitEventMutex.Unlock()
+	eventfuncs.RateLimitEventHandlers = append(eventfuncs.RateLimitEventHandlers, function)
 }
 
-func RegisterTypingStartEventHandler(function func(session *discordgo.Session, event *discordgo.TypingStart)) {
+func RegisterReadyEventHandler(function func(*discordgo.Session, *discordgo.Ready)) {
+	eventfuncs.ReadyEventMutex.Lock()
+	defer eventfuncs.ReadyEventMutex.Unlock()
+	eventfuncs.ReadyEventHandlers = append(eventfuncs.ReadyEventHandlers, function)
 }
 
-func RegisterUserGuildSettingsUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.UserGuildSettingsUpdate)) {
+func RegisterRelationshipAddEventHandler(function func(*discordgo.Session, *discordgo.RelationshipAdd)) {
+	eventfuncs.RelationshipAddEventMutex.Lock()
+	defer eventfuncs.RelationshipAddEventMutex.Unlock()
+	eventfuncs.RelationshipAddEventHandlers = append(eventfuncs.RelationshipAddEventHandlers, function)
 }
 
-func RegisterUserNoteUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.UserNoteUpdate)) {
+func RegisterRelationshipRemoveEventHandler(function func(*discordgo.Session, *discordgo.RelationshipRemove)) {
+	eventfuncs.RelationshipRemoveEventMutex.Lock()
+	defer eventfuncs.RelationshipRemoveEventMutex.Unlock()
+	eventfuncs.RelationshipRemoveEventHandlers = append(eventfuncs.RelationshipRemoveEventHandlers, function)
 }
 
-func RegisterUserSettingsUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.UserSettingsUpdate)) {
+func RegisterResumedEventHandler(function func(*discordgo.Session, *discordgo.Resumed)) {
+	eventfuncs.ResumedEventMutex.Lock()
+	defer eventfuncs.ResumedEventMutex.Unlock()
+	eventfuncs.ResumedEventHandlers = append(eventfuncs.ResumedEventHandlers, function)
 }
 
-func RegisterUserUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.UserUpdate)) {
+func RegisterTypingStartEventHandler(function func(*discordgo.Session, *discordgo.TypingStart)) {
+	eventfuncs.TypingStartEventMutex.Lock()
+	defer eventfuncs.TypingStartEventMutex.Unlock()
+	eventfuncs.TypingStartEventHandlers = append(eventfuncs.TypingStartEventHandlers, function)
 }
 
-func RegisterVoiceServerUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.VoiceServerUpdate)) {
+func RegisterUserGuildSettingsUpdateEventHandler(function func(*discordgo.Session, *discordgo.UserGuildSettingsUpdate)) {
+	eventfuncs.UserGuildSettingsUpdateEventMutex.Lock()
+	defer eventfuncs.UserGuildSettingsUpdateEventMutex.Unlock()
+	eventfuncs.UserGuildSettingsUpdateEventHandlers = append(eventfuncs.UserGuildSettingsUpdateEventHandlers, function)
 }
 
-func RegisterVoiceStateUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.VoiceStateUpdate)) {
+func RegisterUserNoteUpdateEventHandler(function func(*discordgo.Session, *discordgo.UserNoteUpdate)) {
+	eventfuncs.UserNoteUpdateEventMutex.Lock()
+	defer eventfuncs.UserNoteUpdateEventMutex.Unlock()
+	eventfuncs.UserNoteUpdateEventHandlers = append(eventfuncs.UserNoteUpdateEventHandlers, function)
 }
 
-func RegisterWebhooksUpdateEventHandler(function func(session *discordgo.Session, event *discordgo.WebhooksUpdate)) {
+func RegisterUserSettingsUpdateEventHandler(function func(*discordgo.Session, *discordgo.UserSettingsUpdate)) {
+	eventfuncs.UserSettingsUpdateEventMutex.Lock()
+	defer eventfuncs.UserSettingsUpdateEventMutex.Unlock()
+	eventfuncs.UserSettingsUpdateEventHandlers = append(eventfuncs.UserSettingsUpdateEventHandlers, function)
+}
+
+func RegisterUserUpdateEventHandler(function func(*discordgo.Session, *discordgo.UserUpdate)) {
+	eventfuncs.UserUpdateEventMutex.Lock()
+	defer eventfuncs.UserUpdateEventMutex.Unlock()
+	eventfuncs.UserUpdateEventHandlers = append(eventfuncs.UserUpdateEventHandlers, function)
+}
+
+func RegisterVoiceServerUpdateEventHandler(function func(*discordgo.Session, *discordgo.VoiceServerUpdate)) {
+	eventfuncs.VoiceServerUpdateEventMutex.Lock()
+	defer eventfuncs.VoiceServerUpdateEventMutex.Unlock()
+	eventfuncs.VoiceServerUpdateEventHandlers = append(eventfuncs.VoiceServerUpdateEventHandlers, function)
+}
+
+func RegisterVoiceStateUpdateEventHandler(function func(*discordgo.Session, *discordgo.VoiceStateUpdate)) {
+	eventfuncs.VoiceStateUpdateEventMutex.Lock()
+	defer eventfuncs.VoiceStateUpdateEventMutex.Unlock()
+	eventfuncs.VoiceStateUpdateEventHandlers = append(eventfuncs.VoiceStateUpdateEventHandlers, function)
+}
+
+func RegisterWebhooksUpdateEventHandler(function func(*discordgo.Session, *discordgo.WebhooksUpdate)) {
+	eventfuncs.WebhooksUpdateEventMutex.Lock()
+	defer eventfuncs.WebhooksUpdateEventMutex.Unlock()
+	eventfuncs.WebhooksUpdateEventHandlers = append(eventfuncs.WebhooksUpdateEventHandlers, function)
 }
